b77e9dcb03f513a517baa4bb01ca2b48ed054e24
Lucas Rumney <lucasrumney94@gmail.com>
2016-08-22 18:31:41 -0400

    updated flight script speed

--- Jet Drift Vive Template/Assets/basicTranslationalFlight.cs
+++ Jet Drift Vive Template/Assets/basicTranslationalFlight.cs
@@ -1,15 +1,35 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class basicTranslationalFlight : MonoBehaviour {
 
+    public float thrustStrength = 1.0f;
+
+    private bool translateControlsActive;
+    private Transform TranslateSphereTransform;
+    private Rigidbody CockpitRigidbody;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        translateControlsActive = false;
+        TranslateSphereTransform = GameObject.FindGameObjectWithTag("TranslateSphere").transform;
+	    CockpitRigidbody = GameObject.FindGameObjectWithTag("Cockpit").GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        if (translateControlsActive)
+        {
+            CockpitRigidbody.AddForce(thrustStrength * (TranslateSphereTransform.position - this.gameObject.transform.position));
+        }    
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void onTriggerEnter(Collider c)
+    {
+        if (c.tag == "TranslateSphere")
+        {
+            translateControlsActive = true;
+        } 
+    }
 }
